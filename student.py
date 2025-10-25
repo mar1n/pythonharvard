47ed06ce726214600ff10ec315285795d1dbea22
mar1n <cykcykacz@gmail.com>
2025-10-25 22:04:44 +0100

    part8

---
+++ student.py
@@ -0,0 +1,26 @@
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+    
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+    
+    @classmethod
+    def get(cls):
+        name = input("Name: ")
+        house = input("House: ")
+        return cls(name, house)
+
+def main():
+    student = Student.get()
+    # print(f"{student.name} from {student.house}")
+    print(student)
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
+
+if __name__ == "__main__":
+    main()
